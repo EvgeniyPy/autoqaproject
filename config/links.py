32a99df4d06b85c16cddadb8212d1685781d229e
EvgenArt <evgniy.artamonov@yahoo.com>
2024-02-10 18:54:32 +0300

    first ci

---
+++ config/links.py
@@ -0,0 +1,9 @@
+
+
+
+class Links:
+
+    HOST = "https://opensource-demo.orangehrmlive.com/web/index.php"
+    LOGIN_PAGE = f"{HOST}/auth/login"
+    DUSHBOARD_PAGE = f"{HOST}/dashboard/index"
+    PERSONAl_PAGE = f"{HOST}/pim/viewPersonalDetails/empNumber/7"
---
+++ pages/dashboard_page.py
@@ -0,0 +1,16 @@
+import allure
+from base.base_page import BasePage
+from config.links import Links
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+class DashboardPage(BasePage):
+
+    PAGE_URL = Links.DUSHBOARD_PAGE
+
+    MY_INFO_BUTTON = ("xpath", "//span[text()='My Info']")
+
+    @allure.step("Click on  'My Info' page")
+    def click_my_info_button(self):
+        self.wait.until(EC.element_to_be_clickable(self.MY_INFO_BUTTON)).click()
